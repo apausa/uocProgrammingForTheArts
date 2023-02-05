1142eda8abd64c4a6bc17f4b2e9ca5b61b034eb0
Pablo Apausa <pabloapausa@outlook.com>
2023-02-05 22:16:22 +0100

    Remake code

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,16 @@
 function setup() {
-    // put setup code here
+  createCanvas(windowWidth, windowHeight);
+}
+ 
+function draw() {
+  background(0);
+  noStroke();
+
+  for (let i = 0; i < 8; i++) {
+    let alpha = 64 + i;
+    let size = (width / 2) - (i * 32);
+ 
+    fill(255, 255, 255, alpha);
+    ellipse(width / 2, height / 2, size, size);
   }
-  
-  function draw() {
-    // put drawing code here
-  }
-  
+}
